20e2951e281f4516ae203b6736d659d0a9449682
Pedro Henrique <pedrohenriquenasck@gmail.com>
2025-11-07 11:15:41 -0300

    Implement name input validation with toast message
    Added validation for empty name input and a toast message.

--- app/src/main/java/com/example/animais/MainActivity.kt
+++ app/src/main/java/com/example/animais/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.animais
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.animais.databinding.ActivityMainBinding
 
@@ -18,11 +19,19 @@ class MainActivity : AppCompatActivity() {
 
             val nome = binding.nome.text.toString()
 
-            val intent = Intent(this, SecondActivity::class.java) // Abre a SecondActivity
+            if(nome.isEmpty()) {
+                toast("Preencha o nome.")
+            } else {
+                val intent = Intent(this, SecondActivity::class.java) // Abre a SecondActivity
 
-            intent.putExtra("NOME", nome) // A chave "NOME" é como um rótulo para a caixa
+                intent.putExtra("NOME", nome) // A chave "NOME" é como um rótulo para a caixa
 
-            startActivity(intent) // Inicia a nova tela
+                startActivity(intent) // Inicia a nova tela
+            }
         }
+    }
+
+    private fun toast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
